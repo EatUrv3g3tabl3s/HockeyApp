83014408cdcb01e680ca5c88b9f8809c0f2474a6
EatUrv3g3tabl3s <v3gas.o.g@gmail.com>
2014-08-27 21:18:28 -0500

    buzzer beater, seconds fixed to updated time

--- app/src/main/java/com/hockey/hockeyapp/ui/fragments/BuzzerBeater.java
+++ app/src/main/java/com/hockey/hockeyapp/ui/fragments/BuzzerBeater.java
@@ -3,14 +3,17 @@ package com.hockey.hockeyapp.ui.fragments;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hockey.hockeyapp.R;
+import com.hockey.hockeyapp.ui.adapters.TextWatcherAdapter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,30 +23,48 @@ import java.util.concurrent.TimeUnit;
 public class BuzzerBeater extends Fragment {
 
     private Button btnStart, btnStop;
-    private TextView textViewTime;
+    private EditText textViewTime;
+    private int timeEntered;
+    private CounterClass timer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.buzzer_beater, container, false);
+        final View rootView = inflater.inflate(R.layout.buzzer_beater, container, false);
 
         btnStart = (Button) rootView.findViewById(R.id.btnStart);
         btnStop = (Button) rootView.findViewById(R.id.btnStop);
-        textViewTime = (TextView) rootView.findViewById(R.id.textViewTime);
+        textViewTime = (EditText) rootView.findViewById(R.id.textViewTime);
 
-        textViewTime.setText("");
+        timeEntered = Integer.parseInt(textViewTime.getText().toString());
 
-        final CounterClass timer = new CounterClass(5000, 100);
+
+        textViewTime.addTextChangedListener(new TextWatcherAdapter());
+
+
+
+        System.out.println(timeEntered);
+
+
+        //CounterClass timer = new CounterClass(timeEntered * 1000, 100);
+       // final CounterClass timer = new CounterClass(100);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                timeEntered = Integer.parseInt(textViewTime.getText().toString());
+              //  System.out.println(timeEntered);
+                timer = new CounterClass(timeEntered * 1000, 100);
                 timer.start();
+
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //timeEntered = Integer.parseInt(textViewTime.getText().toString());
+
+                //CounterClass timer = new CounterClass(timeEntered*1000, 100);
                 timer.cancel();
             }
         });
@@ -60,7 +81,7 @@ public class BuzzerBeater extends Fragment {
         @Override
         public void onFinish()
         {
-            textViewTime.setText("0 seconds");
+            textViewTime.setText("00");
         }
 
         @Override
@@ -70,7 +91,10 @@ public class BuzzerBeater extends Fragment {
             if(Math.round((float)ms / 1000.0f) != secondsLeft)
             {
                 secondsLeft = Math.round((float)ms / 1000.0f);
-                textViewTime.setText(secondsLeft + "seconds");
+                if(secondsLeft < 10)
+                    textViewTime.setText("0" + String.valueOf(secondsLeft));
+                else
+                    textViewTime.setText(String.valueOf(secondsLeft));
             }
 
             Log.i("test","ms="+ms+" till finished="+secondsLeft );
